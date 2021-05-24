3e394ad23ad7ff2454f5852da5a1580e6faba29c
klenkes74 <rlichti@kaiserpfalz-edv.de>
2021-05-24 16:45:18 +0200

    Moved OffsetDateTimeCodecProvider to kp-commons-core
    Signed-off-by: klenkes74 <rlichti@kaiserpfalz-edv.de>

---
+++ kp-commons-core/src/main/java/de/kaiserpfalzedv/commons/core/mongodb/OffsetDateTimeCodecProvider.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021 Kaiserpfalz EDV-Service, Roland T. Lichti.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.kaiserpfalzedv.commons.core.mongodb;
+
+import io.github.cbartosiak.bson.codecs.jsr310.offsetdatetime.OffsetDateTimeAsStringCodec;
+import org.bson.codecs.Codec;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+
+import java.time.OffsetDateTime;
+
+/**
+ * OffsetDateTimeCodecProvider --
+ *
+ * @author klenkes74 {@literal <rlichti@kaiserpfalz-edv.de>}
+ * @since 1.2.0  2021-05-23
+ */
+public class OffsetDateTimeCodecProvider implements CodecProvider {
+    @Override
+    public <T> Codec<T> get(Class<T> clazz, CodecRegistry registry) {
+        if (clazz == OffsetDateTime.class) {
+            //noinspection unchecked
+            return (Codec<T>) new OffsetDateTimeAsStringCodec();
+        }
+
+        return null;
+    }
+}
