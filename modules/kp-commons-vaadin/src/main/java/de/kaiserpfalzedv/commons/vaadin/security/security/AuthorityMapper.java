be2908fbda3ff426b91fd909b213f12209fff9e8
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2023-12-25 18:46:06 +0100

    initial structure

---
+++ modules/kp-commons-vaadin/src/main/java/de/kaiserpfalzedv/commons/vaadin/security/security/AuthorityMapper.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.kaiserpfalzedv.commons.vaadin.security.security;
+
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+
+/**
+ * 
+ * @author klenkes74 {@literal <rlichti@kaiserpfalz-edv.de>}
+ * @version 1.0.0
+ * @since 2023-12-25
+ */
+@ToString(onlyExplicitlyIncluded = true, includeFieldNames = true)
+@Slf4j
+@EnableWebSecurity
+public class AuthorityMapper {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+            .mvcMatchers("/my-endpoint")
+                .hasAuthority("SCOPE_openid")
+            .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+}
