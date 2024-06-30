172c6c1cf6c0d3147d0cb228f3a33a1b0b8c65b2
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2024-06-30 10:50:20 +0200

    removed "su" from run-java.sh

--- kp-java-runner/run-java.sh
+++ kp-java-runner/run-java.sh
@@ -683,11 +683,7 @@ run() {
   # However it could be two args (see above). zsh doesn't like this btw, but zsh is not
   # supported anyway.
   echo exec $(exec_args) java $(java_options) -cp "$(classpath)" ${args} "$@"
-    if [ "${RUN_AS_ROOT_USER:-'false'}" = 'true' ]; then
-        exec $(exec_args) java $(java_options) -cp "$(classpath)" ${args} "$@"
-    else
-        exec su -s /bin/bash -c "java $(java_options) -cp "$(classpath)" ${args} "$@"" appuser
-    fi
+  exec $(exec_args) java $(java_options) -cp "$(classpath)" ${args} "$@"
 
 }
 
