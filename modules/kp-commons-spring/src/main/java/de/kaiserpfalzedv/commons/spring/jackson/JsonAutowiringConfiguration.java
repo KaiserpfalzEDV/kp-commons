748f2677dfe59c3646347472d1fe0edbb420d6ba
klenkes74 <rlichti@kaiserpfalz-edv.de>
2024-09-22 08:49:53 +0200

    Moved everything from jackson module to the spring module

--- modules/kp-commons-spring/src/main/java/de/kaiserpfalzedv/commons/spring/jackson/JsonAutowiringConfiguration.java
+++ modules/kp-commons-spring/src/main/java/de/kaiserpfalzedv/commons/spring/jackson/JsonAutowiringConfiguration.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package de.kaiserpfalzedv.commons.jackson;
+package de.kaiserpfalzedv.commons.spring.jackson;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
