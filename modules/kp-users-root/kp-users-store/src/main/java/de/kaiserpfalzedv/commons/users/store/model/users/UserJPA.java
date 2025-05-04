72d0f5b289cd7d96ef2ad0cd58304c63de422baf
klenkes74 <rlichti@kaiserpfalz-edv.de>
2025-05-04 23:52:29 +0200

    Added the local JPA stuff

---
+++ modules/kp-users-root/kp-users-store/src/main/java/de/kaiserpfalzedv/commons/users/store/model/users/UserJPA.java
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2025. Roland T. Lichti, Kaiserpfalz EDV-Service.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package de.kaiserpfalzedv.commons.users.store.model.users;
+
+import com.google.common.eventbus.EventBus;
+import de.kaiserpfalzedv.commons.jpa.AbstractRevisionedJPAEntity;
+import de.kaiserpfalzedv.commons.users.domain.model.events.state.UserBannedEvent;
+import de.kaiserpfalzedv.commons.users.domain.model.events.state.UserDetainedEvent;
+import de.kaiserpfalzedv.commons.users.domain.model.events.state.UserReleasedEvent;
+import de.kaiserpfalzedv.commons.users.domain.model.events.state.UserDeletedEvent;
+import de.kaiserpfalzedv.commons.users.domain.model.user.User;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.time.*;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * The player
+ * @author klenkes74 {@literal <rlichti@kaiserpfalz-edv.de>}
+ * @since 1.0.0
+ */
+@SuppressWarnings("JpaDataSourceORMInspection")
+@Entity
+@Table(
+    name = "USERS",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"ID"}),
+        @UniqueConstraint(columnNames = {"ISSUER", "SUBJECT"}),
+        @UniqueConstraint(columnNames = {"NAMESPACE", "NAME"})
+    }
+)
+@Jacksonized
+@SuperBuilder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@ToString(callSuper = true, onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(callSuper = true)
+@XSlf4j
+public class UserJPA extends AbstractRevisionedJPAEntity<UUID> implements User {
+    @Nullable
+    @Column(name = "DETAINED_DURATION")
+    private Duration detainmentDuration;
+    
+    @Nullable
+    @Column(name = "DETAINED_TILL")
+    private OffsetDateTime detainedTill;
+    
+    @NotNull
+    @Column(name = "BANNED", nullable = false)
+    private OffsetDateTime bannedOn;
+    
+    @Transient
+    public boolean isBanned() {
+        return bannedOn != null;
+    }
+    
+    
+    
+    @NotNull
+    @NotBlank
+    @Column(name = "ISSUER", nullable = false)
+    private String issuer;
+    
+    @NotNull
+    @NotBlank
+    @Column(name = "SUBJECT", nullable = false)
+    private String subject;
+    
+    /** The namespace this player is registered for. */
+    @NotNull
+    @Column(name = "NAMESPACE", columnDefinition = "VARCHAR(100)", nullable = false, updatable = false)
+    @Size(min = 3, max = 100, message = "The length of the string must be between 3 and 100 characters long.") @Pattern(regexp = "^[a-zA-Z][-a-zA-Z0-9]{1,61}(.[a-zA-Z][-a-zA-Z0-9]{1,61}){0,4}$", message = "The string must match the pattern '^[a-zA-Z][-a-zA-Z0-9]{1,61}(.[a-zA-Z][-a-zA-Z0-9]{1,61}){0,4}$'")
+    @ToString.Include
+    private String nameSpace;
+
+    /** The name of the player. Needs to be unique within the namespace. */
+    @NotNull
+    @Column(name = "NAME", columnDefinition = "VARCHAR(100)", nullable = false)
+    @Size(min = 3, max = 100, message = "The length of the string must be between 3 and 100 characters long.") @Pattern(regexp = "^[a-zA-Z][-a-zA-Z0-9]{1,61}(.[a-zA-Z][-a-zA-Z0-9]{1,61}){0,4}$", message = "The string must match the pattern '^[a-zA-Z][-a-zA-Z0-9]{1,61}(.[a-zA-Z][-a-zA-Z0-9]{1,61}){0,4}$'")
+    @ToString.Include
+    private String name;
+    
+    @Nullable
+    @Column(name = "EMAIL", columnDefinition = "VARCHAR(100)")
+    private String email;
+    
+    @Nullable
+    @Column(name = "PHONE", columnDefinition = "VARCHAR(100)")
+    private String phone;
+    
+    @Nullable
+    @Column(name = "DISCORD", columnDefinition = "VARCHAR(100)")
+    private String discord;
+    
+    
+    // FIXME 2025-05-04 klenkes74 Repair the JPA handling of the authorities ...
+    private Set<GrantedAuthority> authorities;
+    
+    
+    @Override
+    public UserJPA detain(@NotNull EventBus bus, @Min(1)@Max(1095) long days) {
+        log.entry(days);
+        
+        detainmentDuration = Duration.ofDays(days);
+        
+        detainedTill = LocalDate.now()
+            .atStartOfDay(ZoneId.of("UTC"))
+            .plusDays(1+ days) // today end of day (1) + days
+            .toOffsetDateTime();
+        
+        bus.post(UserDetainedEvent.builder().user(this).days(days).build());
+        
+        return log.exit(this);
+    }
+    
+    @Override
+    public UserJPA release(@NotNull EventBus bus) {
+        log.entry();
+        
+        detainmentDuration = null;
+        detainedTill = null;
+        
+        bus.post(UserReleasedEvent.builder().user(this).build());
+        
+        return log.exit(this);
+    }
+    
+    @Override
+    public UserJPA ban(@NotNull EventBus bus) {
+        log.entry();
+        
+        this.bannedOn = OffsetDateTime.now();
+        
+        bus.post(UserBannedEvent.builder().user(this).timestamp(bannedOn).build());
+        
+        return log.exit(this);
+    }
+    
+    @Override
+    public UserJPA delete(@NotNull EventBus bus) {
+        log.entry();
+        
+        this.deleted = OffsetDateTime.now(Clock.systemUTC());
+        
+        bus.post(UserDeletedEvent.builder().user(this).timestamp(deleted).build());
+        
+        return log.exit(this);
+    }
+    
+    @Override
+    public UserJPA undelete(@NotNull EventBus bus) {
+        log.entry();
+        
+        this.deleted = null;
+        
+        bus.post(UserReleasedEvent.builder().user(this).build());
+        
+        return log.exit(this);
+    }
+
+    
+    @Override
+    public void eraseCredentials() {
+        // do nothing. We don't have credentials ...
+    }
+}
--- modules/kp-users-root/kp-users-store/src/main/java/de/kaiserpfalzedv/commons/users/store/service/KpUserStoreService.java
+++ modules/kp-users-root/kp-users-store/src/main/java/de/kaiserpfalzedv/commons/users/store/service/KpUserStoreService.java
@@ -1,14 +1,38 @@
-package de.kaiserpfalzedv.commons.users.store.service;
+/*
+ * Copyright (c) 2024-2025. Roland T. Lichti, Kaiserpfalz EDV-Service.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+package de.kaiserpfalzedv.commons.users.store.service;
 
 import de.kaiserpfalzedv.commons.users.domain.UserCantBeCreatedException;
 import de.kaiserpfalzedv.commons.users.domain.model.user.User;
 import de.kaiserpfalzedv.commons.users.domain.services.UserReadService;
 import de.kaiserpfalzedv.commons.users.domain.services.UserWriteService;
+import de.kaiserpfalzedv.commons.users.store.model.users.UserJPA;
+import de.kaiserpfalzedv.commons.users.store.model.users.UserRepository;
+import de.kaiserpfalzedv.commons.users.store.model.users.UserToJpa;
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
 import jakarta.inject.Inject;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.XSlf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -16,87 +40,163 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-/**
- * @author klenkes74 {@literal <rlichti@kaiserpfalz-edv.de>}
- * @since 03.05.2025
- */
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Inject))
+@ToString(callSuper = true, onlyExplicitlyIncluded = true)
 @XSlf4j
 public class KpUserStoreService implements UserReadService, UserWriteService {
-  
-  @Override
-  public Optional<User> findById(final UUID id) {
-    return Optional.empty();
-  }
-  
-  @Override
-  public Optional<User> findByUsername(final String nameSpace, final String name) {
-    return Optional.empty();
-  }
-  
-  @Override
-  public Optional<User> findByEmail(final String email) {
-    return Optional.empty();
-  }
-  
-  @Override
-  public Optional<User> findByOauth(final String issuer, final String sub) {
-    return Optional.empty();
-  }
-  
-  @Override
-  public List<User> findAllByNamespace(final String namespace) {
-    return List.of();
-  }
-  
-  @Override
-  public Page<User> findAllByNamespace(final String namespace, final Pageable pageable) {
-    return null;
-  }
-  
-  @Override
-  public List<User> findAllByIssuer(final String issuer) {
-    return List.of();
-  }
-  
-  @Override
-  public Page<User> findAllByIssuer(final String issuer, final Pageable pageable) {
-    return null;
-  }
-  
-  @Override
-  public User create(final User user) throws UserCantBeCreatedException {
-    throw new UserCantBeCreatedException(user.getIssuer(), user.getSubject(), user.getUsername(), user.getEmail());
-  }
-  
-  @Override
-  public User updateIssuer(final User user, final String issuer, final String sub) {
-    return null;
-  }
-  
-  @Override
-  public User updateNamespace(final User user, final String namespace) {
-    return null;
-  }
-  
-  @Override
-  public User updateName(final User user, final String name) {
-    return null;
-  }
-  
-  @Override
-  public User updateNamespaceAndName(final User user, final String namespace, final String name) {
-    return null;
-  }
-  
-  @Override
-  public User updateEmail(final User user, final String email) {
-    return null;
-  }
-  
-  @Override
-  public User updateDiscord(final User user, final String discord) {
-    return null;
-  }
+    private final UserRepository users;
+    private final UserToJpa toUserJPA;
+
+
+    @Override
+    @Counted
+    @Timed
+    public Optional<User> findById(final UUID id) {
+        log.entry(id);
+        
+        Optional<UserJPA> result = users.findById(id);
+        
+        return log.exit(Optional.ofNullable(result.orElse(null)));
+    }
+    
+    @Override
+    @Counted
+    @Timed
+    public Optional<User> findByUsername(final String namespace, final String name) {
+        log.entry(namespace, name);
+        
+        Optional<UserJPA> result = users.findByNameSpaceAndName(namespace, name);
+        
+        return log.exit(Optional.ofNullable(result.orElse(null)));
+    }
+    
+    @Override
+    public Optional<User> findByEmail(final String email) {
+        log.entry(email);
+        
+        Optional<UserJPA> result = users.findByEmail(email);
+        
+        return log.exit(Optional.ofNullable(result.orElse(null)));
+    }
+    
+    @Override
+    public Optional<User> findByOauth(final String issuer, final String sub) {
+        log.entry(issuer, sub);
+        
+        Optional<UserJPA> result = users.findByIssuerAndSubject(issuer, sub);
+        
+        return log.exit(Optional.ofNullable(result.orElse(null)));
+    }
+    
+    @Override
+    public List<User> findAllByNamespace(final String namespace) {
+        log.entry(namespace);
+        
+        List<UserJPA> data = users.findByNameSpace(namespace);
+        
+        return log.exit(data.stream().map(e -> (User)e).toList());
+    }
+    
+    @Override
+    public Page<User> findAllByNamespace(final String namespace, final Pageable pageable) {
+        log.entry(namespace, pageable);
+        
+        Page<UserJPA> result = users.findByNameSpace(namespace, pageable);
+        List<User> data = result.stream().map(e -> (User)e).toList();
+        
+        return log.exit(new PageImpl<>(data, result.getPageable(), result.getTotalElements()));
+    }
+    
+    @Override
+    public List<User> findAllByIssuer(final String issuer) {
+        log.entry(issuer);
+        
+        List<UserJPA> data = users.findByIssuer(issuer);
+        
+        return log.exit(data.stream().map(e -> (User)e).toList());
+    }
+    
+    @Override
+    public Page<User> findAllByIssuer(final String issuer, final Pageable pageable) {
+        log.entry(issuer, pageable);
+        
+        Page<UserJPA> result = users.findByIssuer(issuer, pageable);
+        List<User> data = result.stream().map(e -> (User)e).toList();
+        
+        return log.exit(new PageImpl<>(data, result.getPageable(), result.getTotalElements()));
+    }
+    
+    @Override
+    public User create(final User user) throws UserCantBeCreatedException {
+        log.entry(user);
+        
+        try {
+            UserJPA data = users.save(toUserJPA.apply(user));
+            
+            return log.exit(data);
+        } catch (IllegalArgumentException | OptimisticLockingFailureException e) {
+            throw new UserCantBeCreatedException(user.getIssuer(), user.getSubject(), user.getUsername(), user.getEmail(), e);
+        }
+    }
+    
+    @Override
+    public User updateIssuer(final User user, final String issuer, final String sub) {
+        log.entry(issuer, sub);
+        
+        Optional<UserJPA> data = users.findById(user.getId());
+        data.ifPresent(e -> users.save(e.toBuilder().issuer(issuer).build()));
+
+        return log.exit(users.findById(user.getId()).orElse(null));
+    }
+    
+    @Override
+    public User updateNamespace(final User user, final String namespace) {
+        log.entry(user, namespace);
+        
+        Optional<UserJPA> data = users.findById(user.getId());
+        data.ifPresent(e -> users.save(e.toBuilder().nameSpace(namespace).build()));
+
+        return log.exit(users.findById(user.getId()).orElse(null));
+    }
+    
+    @Override
+    public User updateName(final User user, final String name) {
+        log.entry(user, name);
+        
+        Optional<UserJPA> data = users.findById(user.getId());
+        data.ifPresent(e -> users.save(e.toBuilder().name(name).build()));
+        
+        return log.exit(users.findById(user.getId()).orElse(null));
+    }
+    
+    @Override
+    public User updateNamespaceAndName(final User user, final String namespace, final String name) {
+        log.entry(user, namespace, name);
+        
+        Optional<UserJPA> data = users.findById(user.getId());
+        data.ifPresent(e -> users.save(e.toBuilder().nameSpace(namespace).name(name).build()));
+        
+        return log.exit(users.findById(user.getId()).orElse(null));
+    }
+    
+    @Override
+    public User updateEmail(final User user, final String email) {
+        log.entry(user, email);
+        
+        Optional<UserJPA> data = users.findById(user.getId());
+        data.ifPresent(e -> users.save(e.toBuilder().email(email).build()));
+        
+        return log.exit(users.findById(user.getId()).orElse(null));
+    }
+    
+    @Override
+    public User updateDiscord(final User user, final String discord) {
+        log.entry(user, discord);
+        
+        Optional<UserJPA> data = users.findById(user.getId());
+        data.ifPresent(e -> users.save(e.toBuilder().discord(discord).build()));
+        
+        return log.exit(users.findById(user.getId()).orElse(null));
+    }
 }
