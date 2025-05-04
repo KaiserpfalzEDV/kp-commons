1a750e7a06607859da063dc6aafe43acb5997573
klenkes74 <rlichti@kaiserpfalz-edv.de>
2025-05-04 23:52:29 +0200

    Added the local JPA stuff

---
+++ modules/kp-users-root/kp-users-store/src/main/java/de/kaiserpfalzedv/commons/users/store/model/EnableUserStore.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024-2025. Kaiserpfalz EDV-Service, Roland T. Lichti
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or  (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.kaiserpfalzedv.commons.users.store.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.lang.annotation.*;
+
+
+/**
+ * 
+ * @author klenkes74 {@literal <rlichti@kaiserpfalz-edv.de>}
+ * @version 1.0.0
+ * @since 2024-09-27
+ */
+@SuppressWarnings("unused")
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@EnableJpaRepositories
+@EntityScan(basePackages = "de.paladinsinn.tp.dcis.users.store")
+@ComponentScan(basePackages = "de.paladinsinn.tp.dcis.users.store")
+public @interface EnableUserStore {}
